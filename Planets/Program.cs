4890bf9e1914b4a589f2c17236fb8281f6d4cd2f
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-23 21:16:10 -0600

    planets are starting to write

---
+++ Planets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.AddRange(new List<string> { "Jupiter", "Saturn" });
+
+            List<string> morePlanets = new List<string>() { "Neptune", "Pluto" };
+            planetList.AddRange(morePlanets);
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
